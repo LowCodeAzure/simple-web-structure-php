a45f34c5d22b8abaed5917c8b08ab1481cd14974
Edwin Ashdown <edwin@lowcodeazure.com>
2021-10-02 17:14:34 +1300

    Update loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -10,9 +10,6 @@ require_once(ROOT_DIR.'includes/functions.php');
 # Check database
 checkDatabaseInstallation();
 
-# Check logged user
-if (!isLoggedIn() && !preg_match('/login(.php)?/', currentPage())) {
-  redirectTo(ROOT_DIR.'login.php');
-}
+
 
 ?>
